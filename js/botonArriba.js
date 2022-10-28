f5055687a4627b3383da3eb98e672170e5c509ec
RandyGD <randygarciadaniel@gmail.com>
2022-10-27 19:04:51 -0900

    Catalogo PKT

---
+++ js/botonArriba.js
@@ -0,0 +1,18 @@
+function irArriba(){
+	window.addEventListener('scroll', () => {
+		var scroll = document.documentElement.scrollTop;
+		console.log(scroll);
+		var botonArriba = document.getElementById('botonArriba');
+
+		if (scroll > 500){
+			botonArriba.style.right = 20 + "px";
+		} else {
+			botonArriba.style.right = -100 + "px";
+		}
+
+
+
+	})
+}
+
+irArriba();
---
+++ js/notificacion.js
@@ -0,0 +1,67 @@
+Swal.fire({
+    title: "Bienvenido!",
+    text: "Gracias por visitar nuestro sitio web, disfruta de los mejores catalogos disponibles",
+	
+	
+	// html:
+	icon: "success",
+	confirmButtonText: "Continuar",
+	// footer: "Derechos reservados 2022&copy; Randy(GD)"
+	// width: "50%"
+	// padding:
+	// background:
+	// grow:
+	// backdrop:
+	timer: "10000",
+	timerProgressBar: "true",
+	
+	
+	// toast:
+	// position:
+	allowOutsideClick: true,
+	allowEscapeKey: true,
+	allowEnterKey: true,
+	stopKeydownPropagation: false,
+
+	// input: 
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:  
+	// 	cancelButton:
+	// 	footer:	
+
+	// showConfirmButton:
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	showCancelButton: false,
+	ancelButtonText: "Cancelar",
+	//cancelButtonColor:
+	cancelButtonAriaLabel: "Cancelar",
+	
+	// buttonsStyling:
+	showCloseButton: true,
+	closeButtonAriaLabel: "Cerrar ventana",
+
+
+	// imageUrl:
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+
+
+
+});
